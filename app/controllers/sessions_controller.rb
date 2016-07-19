1beadefeaa2059139f35bccfd6d5f4def7f16d99
James <jamesnguyen23@gmail.com>
2016-07-19 16:59:38 -0400

    user edit, update, index, and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       log_in(@user)
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       flash[:success] = "Welcome #{@user.name}"
-      redirect_to @user
+      redirect_back_or @user
     else
       flash.now[:danger] = "Invalid email/password combination"
       render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,11 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
+
   def current_user
     #look for user logged via session
     #set user id to session[:user_id] if session exists
@@ -44,5 +49,16 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
