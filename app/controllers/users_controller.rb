19566627d8321a6586dfac195dad1480acd98205
James <jamesnguyen23@gmail.com>
2016-07-21 21:58:13 -0400

    added user posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
     redirect_to root_url and return unless logged_in?
   end
 
@@ -50,15 +51,6 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                     :password_confirmation)
-    end
-
-    # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
     end
 
     # Confirms the correct user.
--- db/seeds.rb
+++ db/seeds.rb
@@ -25,3 +25,9 @@ User.create!(name:  "James Nguyen",
                activated: true,
                activated_at: Time.zone.now)
 end
+
+users = User.order(:created_at).take(30)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
